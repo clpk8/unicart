91d90f4a9311a144a5afc2150c675e3d777ce058
Roy <royxuhan@gmail.com>
2021-04-20 01:16:03 -0400

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
 
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages';
-import Buy from './pages/buy';
-import Sell from './pages/sell';
-import Transactions from './pages/transactions';
-import ForSale from './pages/forSale';
-import Account from './pages/account';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import Home from './pages';
+import Footer from './components/Footer';
+// import Buy from './pages/buy';
+// import Sell from './pages/sell';
+// import Transactions from './pages/transactions';
+// import ForSale from './pages/forSale';
+// import Account from './pages/account';
   
 function App() {
   return (
 	<div className='App'>
 		<Router>
       <Navbar />
-      <Switch>
+      <Home />
+
+      {/* <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/buy' component={Buy} />
         <Route path='/sell' component={Sell} />
         <Route path='/transactions' component={Transactions} />
         <Route path='/forSale' component={ForSale} />
         <Route path='/account' component={Account} />
-      </Switch>
+      </Switch> */}
+      <Footer />
 		</Router>
 	</div>
   );
