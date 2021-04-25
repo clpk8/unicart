312a99b8c1d4b228f8aa71f11f69ddb769188541
ajwagner26 <ajwagner@andrew.cmu.edu>
2021-04-25 14:59:20 -0400

    Created basic structure for the sell page

--- client/src/pages/sell.js
+++ client/src/pages/sell.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
-import Banner from '../components/Banner'
+import image1 from '../book.jpg';
+import '../App.css'
 class Sell extends Component {
   render () {
     return (
-      <div id='home'>
-        <Banner />
-      </div>
+      <form>
+        <div class="grid-container">
+        <div class="grid-item"><img src={image1} /></div>
+        <div class="grid-item">
+            <label>
+              Item Title:
+              <input type="text" name="name" />
+            </label>
+            <label>
+              Short Description:
+              <input type="text" name="name" />
+            </label>
+            <label>
+              price:
+              <input type="text" name="name" />
+            </label>
+          
+        </div>
+        <div class="grid-item">
+          <label>
+            Full Description:
+            <input type="text" name="name" />
+          </label>
+        </div>
+        <div class="grid-item">
+          <input type="submit" value="Post" />
+        </div>
+        </div>
+      </form>
     )
   }
 }
