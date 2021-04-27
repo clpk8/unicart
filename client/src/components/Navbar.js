c0946d9e5cd18c0c6b4752dcd7973d903e48c0a4
Roy <royxuhan@gmail.com>
2021-04-27 02:29:20 -0400

    Nav bar design

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,13 +9,23 @@ class Navbar extends Component {
         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
         <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-        <ul id="nav" className="nav">
-          {/* <li className="current"><a className="smoothscroll"
-          href="/home"><h3>Home</h3></a></li> */}
-          <li><a className="smoothscroll" href="/sell"><h3>Sell</h3></a></li>
-          <li><a className="smoothscroll" href="/signin"><h3>Log In</h3></a></li>
-          <li><a className="smoothscroll" href="/signup"><h3>Sign Up</h3></a></li>
-        </ul>
+        <div className="row">
+          <div className="four columns">
+            <a className="logo" href="/home">
+              <img className="logo-img" src="/assets/logo-circle.png" alt="logo" />
+            </a>
+          </div>
+
+          <div className="eight columns">
+            <ul id="nav" className="nav">
+              {/* <li className="current"><a className="smoothscroll"
+              href="/home"><h3>Home</h3></a></li> */}
+              <li><a className="smoothscroll" href="/sell"><h5>Sell</h5></a></li>
+              <li><a className="smoothscroll" href="/signin"><h5>Log In</h5></a></li>
+              <li><a className="smoothscroll" href="/signup"><h5>Sign Up</h5></a></li>
+            </ul>
+          </div>
+        </div>
       </nav>
     );
   }
