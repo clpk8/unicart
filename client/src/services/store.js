6005b17f4226c5b8521eb03e83437119677c0ddb
Roy <royxuhan@gmail.com>
2021-04-27 02:14:28 -0400

    Sell page fix

--- client/src/services/store.js
+++ client/src/services/store.js
@@ -2,8 +2,16 @@ import { createStore, action } from 'easy-peasy';
 
 const store = createStore({
   cart: [],
+  category: '',
+  condition: '',
   setCart: action((state, payload) => {
     state.cart = payload;
+  }),
+  setCategory: action((state, payload) => {
+    state.category = payload;
+  }),
+  setCondition: action((state, payload) => {
+    state.condition = payload;
   }),
 });
 
