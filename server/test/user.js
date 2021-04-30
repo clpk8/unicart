e208c3782c1438574bdb58c2f72f8d54306d7615
ben <cblin@andrew.cmu.edu>
2021-04-30 10:01:14 -0500

    Added a simple user test

---
+++ server/test/user.js
@@ -0,0 +1,55 @@
+process.env.NODE_ENV = 'test';
+
+const { expect } = require('chai');
+const request = require('supertest');
+const mocha = require('mocha');
+const app = require('../app');
+const db = require('../db');
+
+describe('GET /user', () => {
+  mocha.before((done) => {
+    console.log('connecting db');
+    db.connect().then(() => {
+      done();
+    });
+  });
+
+  mocha.after((done) => {
+    db.close()
+      .then(() => done())
+      .catch((err) => done(err));
+  });
+
+  it('OK, no users found', (done) => {
+    request(app)
+      .get('/users')
+      .then((res) => {
+        const { body } = res;
+        expect(body.length).to.equal(0);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  it('OK, registered a uer and get the user', (done) => {
+    request(app)
+      .post('/api/user/register')
+      .send({
+        firstName: 'test first name',
+        lastName: 'test last name',
+        email: '123@andrew.cmu.edu',
+        password: 'hellohello',
+        school: 'Carnegie Mellon University',
+      })
+      .then(() => {
+        request(app)
+          .get('/users')
+          .then((res) => {
+            const { body } = res;
+            expect(body.length).to.equal(1);
+            done();
+          });
+      })
+      .catch((err) => done(err));
+  });
+});
