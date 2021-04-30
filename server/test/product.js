ceb9c4a42460b9716f7bf585c001224794a12eec
ben <cblin@andrew.cmu.edu>
2021-04-30 10:22:31 -0500

    added test for delete and get individual product

--- server/test/product.js
+++ server/test/product.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 process.env.NODE_ENV = 'test';
 
 const { expect } = require('chai');
@@ -6,6 +7,11 @@ const mocha = require('mocha');
 const app = require('../app');
 const db = require('../db');
 
+const testProduct = {
+  price: 15,
+  title: 'test book for sale',
+  description: 'this is a test',
+};
 describe('GET /products', () => {
   mocha.before((done) => {
     console.log('connecting db');
@@ -34,17 +40,45 @@ describe('GET /products', () => {
   it('OK, created a product and getting the product', (done) => {
     request(app)
       .post('/products')
-      .send({
-        price: 15,
-        title: 'test book for sale',
-        description: 'this is a test',
-      })
+      .send(testProduct)
       .then(() => {
         request(app)
           .get('/products')
           .then((res) => {
             const { body } = res;
             expect(body.length).to.equal(1);
+            done();
+          });
+      })
+      .catch((err) => done(err));
+  });
+
+  it('OK, created a product and the product by id', (done) => {
+    request(app)
+      .post('/products')
+      .send(testProduct)
+      .then((res) => {
+        const id = res.body._id;
+        request(app)
+          .get(`/products/${id}`)
+          .then((productResult) => {
+            expect(productResult.body === testProduct);
+            done();
+          });
+      })
+      .catch((err) => done(err));
+  });
+
+  it('OK, created a product and delete the product', (done) => {
+    request(app)
+      .post('/products')
+      .send(testProduct)
+      .then((res) => {
+        const id = res.body._id;
+        request(app)
+          .delete(`/products/${id}`)
+          .then((deleteResponse) => {
+            expect(deleteResponse.statusCode === 200);
             done();
           });
       })
