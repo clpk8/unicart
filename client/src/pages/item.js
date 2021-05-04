7e056f641311f5b2b41b90f45d02db6cf7c906ec
ajwagner26 <ajwagner@andrew.cmu.edu>
2021-05-04 03:46:00 -0400

    Created format for item page, but not connected to backend

--- client/src/pages/item.js
+++ client/src/pages/item.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useStoreState, useStoreActions } from 'easy-peasy';
+// import { useStoreState, useStoreActions } from 'easy-peasy';
 
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
+// import Select from '@material-ui/core/Select';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import FormControl from '@material-ui/core/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -52,6 +52,7 @@ const useStyles = makeStyles((theme) => ({
 function Item() {
   const classes = useStyles();
 
+  /*
   const category = useStoreState((state) => state.category);
   const setCategory = useStoreActions((actions) => actions.setCategory);
   const condition = useStoreState((state) => state.condition);
@@ -64,7 +65,7 @@ function Item() {
   const handleConditionChange = (event) => {
     setCondition(event.target.value);
   };
-
+*/
   return (
     <section id="Item">
       <div className="row">
@@ -73,72 +74,40 @@ function Item() {
             <CssBaseline />
             <Grid item xs={12} sm={12} md={12} component={Paper} elevation={6} square>
               <div className={classes.paper}>
-                <Typography className={classes.title} component="h1" variant="h5">
-                  Item:
-                </Typography>
-                <form className={classes.form} noValidate>
-                  <Box component="span" m={1}>
-                    <p>Biology Book</p>
-                  </Box>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="price"
-                    label="Price"
-                    type="price"
-                    id="price"
-                  />
-                  <FormControl variant="outlined" className={classes.formControl}>
-                    <InputLabel id="category-select">Category</InputLabel>
-                    <Select
-                      labelId="category-select-label"
-                      id="category-select"
-                      value={category}
-                      onChange={handleCategoryChange}
-                      label="Category"
-                    >
-                      <MenuItem value="books">Books</MenuItem>
-                      <MenuItem value="clothing">Clothing</MenuItem>
-                      <MenuItem value="electronics">Electronics</MenuItem>
-                      <MenuItem value="miscellaneous">Miscellaneous</MenuItem>
-                    </Select>
-                  </FormControl>
-                  <FormControl variant="outlined" className={classes.formControl}>
-                    <InputLabel id="condition-select">Condition</InputLabel>
-                    <Select
-                      labelId="category-select-label"
-                      id="category-select"
-                      value={condition}
-                      onChange={handleConditionChange}
-                      label="Condition"
-                    >
-                      <MenuItem value="new">New</MenuItem>
-                      <MenuItem value="likenew">Used - Like New</MenuItem>
-                      <MenuItem value="good">Used - Good</MenuItem>
-                      <MenuItem value="fair">Used - Fair</MenuItem>
-                    </Select>
-                  </FormControl>
-                  <TextField
-                    id="description"
-                    className={classes.description}
-                    label="Description"
-                    multiline
-                    rows={4}
-                    defaultValue=""
-                    variant="outlined"
-                  />
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                  >
-                    Next
-                  </Button>
-                </form>
+                <Box component="span" m={1}>
+                  <Typography className={classes.title} component="h1" variant="h5">
+                    Item:
+                  </Typography>
+                </Box>
+                <Box component="span" m={1}>
+                  <Typography className={classes.title} component="h1" variant="h5">
+                    Price:
+                  </Typography>
+                </Box>
+                <Box component="span" m={1}>
+                  <Typography className={classes.title} component="h1" variant="h5">
+                    Catagory:
+                  </Typography>
+                </Box>
+                <Box component="span" m={1}>
+                  <Typography className={classes.title} component="h1" variant="h5">
+                    Condition:
+                  </Typography>
+                </Box>
+                <Box component="span" m={1}>
+                  <Typography className={classes.title} component="h1" variant="h5">
+                    Description:
+                  </Typography>
+                </Box>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Add to cart
+                </Button>
               </div>
             </Grid>
           </Grid>
