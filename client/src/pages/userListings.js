e0902352d4ca8e5b7a1655d79fb784c762dac7cb
Shane Dasbach <sedasbac@andrew.cmu.edu>
2021-05-04 01:07:23 -0400

    this seems to be working pretty well

--- client/src/pages/userListings.js
+++ client/src/pages/userListings.js
@@ -1,22 +1,36 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import useFetch from 'react-fetch-hook';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import ProductListing from '../components/productListing';
+
+const useStyles = makeStyles({
+  grid: {
+    margin: 12,
+  },
+  container: {
+    padding: 12,
+  },
+});
 
 function UserProducts(props) {
   const { userid } = props;
+  const classes = useStyles();
 
   const { isLoading, error, data } = useFetch(`/api/products/fetch/${userid}`);
 
   if (isLoading) return 'Loading... One Moment Please.';
   if (error) return 'Error loading this page!';
   const products = data;
-  console.log('id:', userid, 'products:', products);
+  const content = products.map((product) => <ProductListing key={product.id} product={product} />);
   return (
-    <div id="user-listings">
-      {`User ${userid} has ${products.length} product(s)`}
-      <br />
-      Products:
-      {JSON.stringify(products)}
+    <div id="user-products-page" className={classes.container}>
+      <h2>{`Product Listings for user ${userid}`}</h2>
+      <Grid m={12} container spacing={3} className={classes.grid}>
+        {content}
+      </Grid>
+
     </div>
   );
 }
